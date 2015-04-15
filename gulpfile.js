2a924ba93068698eb18d1bc5ba71f1f0d48f1681
Carlos Pinelly <capi@trulii.com>
2015-04-15 16:49:34 -0430

    Tareas de gulp para Less. Seteados estilos

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+
+var gulp = require('gulp');
+var debug = require ( 'gulp-debug' );
+//var watchLess = require('gulp-watch-less');
+var watch = require('gulp-watch');
+var less = require('gulp-less');
+var plumber = require('gulp-plumber');
+
+var path = require('path');
+
+
+
+// *********** Settings ***********
+
+// LESS
+
+var BOOTSTRAP_LESS_FILES_PATH = 'bower_components/bootstrap/less'
+var BOOTSTRAP_MATERIAL_LESS_FILES_PATH = 'bower_components/bootstrap-material-design/less'
+
+var LESS_CONFIG = {
+    paths: [
+        path.join(__dirname, BOOTSTRAP_LESS_FILES_PATH),
+        path.join(__dirname, BOOTSTRAP_MATERIAL_LESS_FILES_PATH)
+    ]
+}
+
+var LESS_OUTPUT = 'public/css'
+
+
+// *********** Tasks ***********
+
+// Watch changes on Less files
+
+gulp.task('less.watch', function () {
+
+    watch('less/**/*.less', function(){
+        gulp.src('less/trulii.less')  // TODO: RegExp for files that do not begin with an underscore
+        .pipe(plumber())
+        .pipe(less(LESS_CONFIG))
+        .pipe(gulp.dest(LESS_OUTPUT));
+    });
+        
+});
+
+// Less files to CSS
+
+gulp.task('less.compile', function () {
+    return gulp.src('less/trulii.less')        
+        .pipe(less(LESS_CONFIG))
+        .pipe(gulp.dest(LESS_OUTPUT));
+});
