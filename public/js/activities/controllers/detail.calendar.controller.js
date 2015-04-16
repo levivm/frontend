a45ca63bee725c13a93da74fc487a3c7104047eb
Harvey Forero <harvey@trulii.com>
2015-04-16 14:03:02 -0500

    TRUL-119 #in-review Botón de inscripción

--- public/js/activities/controllers/detail.calendar.controller.js
+++ public/js/activities/controllers/detail.calendar.controller.js
@@ -14,18 +14,18 @@
         vm.calendar_selected = calendars[0];
 
         uiGmapGoogleMapApi.then(function(maps) {
-                var position = new maps.LatLng(activity.location.point[0], activity.location.point[1]);
-                var gmapOptions = {
-                    zoom: 16,
-                    center: position
-                };
+            var position = new maps.LatLng(activity.location.point[0], activity.location.point[1]);
+            var gmapOptions = {
+                zoom: 16,
+                center: position
+            };
 
-                var gmap = new maps.Map(document.getElementById('map-canvas'), gmapOptions);
+            var gmap = new maps.Map(document.getElementById('map-canvas'), gmapOptions);
 
-                var marker = new maps.Marker({
-                    position: position,
-                    map: gmap
-                });
+            var marker = new maps.Marker({
+                position: position,
+                map: gmap
+            });
         });
     }
 
