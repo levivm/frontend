ded2bcebc41d773fa3bca82c2980e78dfa4b8fdc
Fernando De Freitas <fer@trulii.com>
2015-05-07 18:59:54 -0430

    TRUL-177 #in-review - Maquetada vista de Registro de Organizador

--- app/js/organizers/controllers/landing.controller.js
+++ app/js/organizers/controllers/landing.controller.js
@@ -1,104 +1,103 @@
+/**
+ * @ngdoc controller
+ * @name trulii.organizers.controllers.OrganizerLandingCtrl
+ * @description Handles Organizers Registration requests
+ * @requires ui.router.state.$state
+ * @requires trulii.locations.services.LocationManager
+ * @requires trulii.authentication.services.Authentication
+ * @requires cities
+ */
+
 (function () {
     'use strict';
 
     angular
         .module('trulii.organizers.controllers')
-        .controller('OrganizerLandingCtrl', OrganizerLandingCtrl)
+        .controller('OrganizerLandingCtrl', OrganizerLandingCtrl);
 
-    OrganizerLandingCtrl.$inject = ['$state','LocationManager','Authentication','cities'];
+    OrganizerLandingCtrl.$inject = ['$state', 'LocationManager', 'Authentication', 'cities'];
 
-    function OrganizerLandingCtrl($state,LocationManager,Authentication,cities) {
+    function OrganizerLandingCtrl($state, LocationManager, Authentication, cities) {
+
         var vm = this;
 
+        /**
+         * @name requestSignup
+         * @desc Anonymous user request a organizer signup
+         * @memberOf trulii.organizers.controllers.OrganizerLandingCtrl
+         */
         vm.request_signup = requestSignup;
+
         vm.cities = cities;
         vm.errors = {};
         vm.request = {};
-        vm.strings = {};
-        vm.strings.EMAIL_LABEL = 'Correo Electrónico';
-        vm.strings.PLACEHOLDER_NAME = 'Nombre de Compañía';
-        vm.strings.PLACEHOLDER_TELEPHONE = 'Teléfono';
-        vm.strings.PLACEHOLDER_WANT_TEACH = '¿Qué deseas enseñar?';
-        vm.strings.REQUEST_SIGNUP_LABEL = 'Solicitar registro';
 
-        activate();
+        initialize();
 
 
-
-        /**
-        * @name requestSignup
-        * @desc Anonymous user request a organizer signup
-        * @memberOf trulii.organizers.controllers.OrganizerLandingCtrl
-        */
         function requestSignup() {
-
             _clearErrors();
             vm.request.city = vm.request.current_city.id;
-            Authentication.requestSignup(vm.request).then(successRequestSignup,_errored);
-
-
+            Authentication.requestSignup(vm.request).then(successRequestSignup, _errored);
         }
 
-        function activate(){
-
+        function activate() {
             console.log("activating");
-            LocationManager.getCurrentCity().then(success, error);
-
-            function success(city){
-                vm.request.current_city = city;
-                console.log('getCurrentCity: ', vm.request.current_city);
-            }
-            function error(){
-                console.log("Couldn't obtain current city");
-            }
-
-
+            vm.request.current_city = LocationManager.getCurrentCity();
         }
 
-        function selectCity(city){
+        function selectCity(city) {
             vm.request.current_city = city;
         }
 
-
-        function successRequestSignup(){
-
-            $state.go('general-message',{'module_name':'authentication',
-                           'template_name':'request_signup_success',
-                           'redirect_state':'home'});
-
+        function successRequestSignup() {
+            $state.go('general-message', {
+                'module_name' : 'authentication',
+                'template_name' : 'request_signup_success',
+                'redirect_state' : 'home'
+            });
         }
 
-        function _clearErrors(){
-          vm.errors = null;
-          vm.errors = {};
+        function _clearErrors() {
+            vm.errors = null;
+            vm.errors = {};
         }
-
-
 
         function _addError(field, message) {
-
-          vm.errors[field] = message;
-          vm.pre_signup_form[field].$setValidity(message, false);
-
-        };
-
-
-
-
-        function _errored(response) {
-
-            var errors = response.data;
-            if (errors) {
-
-                angular.forEach(errors, function(errors, field) {
-
-                  _addError(field, errors[0]);
-
-            });
-
-          }
+            vm.errors[field] = message;
+            vm.pre_signup_form[field].$setValidity(message, false);
         }
 
+        function _errored(response) {
+            var errors = response.data;
+            if (errors) {
+                angular.forEach(errors, function (errors, field) {
+                    _addError(field, errors[0]);
+                });
+            }
+        }
+
+        function setStrings() {
+            if (!vm.strings) {
+                vm.strings = {};
+            }
+
+            angular.extend(vm.strings, {
+                PLACEHOLDER_NAME : 'Nombre de Compañía',
+                PLACEHOLDER_TELEPHONE : 'Teléfono',
+                PLACEHOLDER_WANT_TO_TEACH : '¿Qué deseas enseñar?',
+                CITY_LABEL: 'Ciudad',
+                CITY_DEFAULT_LABEL: 'Seleccione Ciudad..',
+                REQUEST_SIGNUP_LABEL : 'Solicitar Registro',
+                EMAIL_LABEL : "Correo electrónico",
+                LOGIN_LABEL : "Inicia Sesión"
+            });
+        }
+
+        function initialize(){
+            setStrings();
+            activate();
+        }
 
     }
 })();
