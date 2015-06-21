e77fef8303a3de425a864421eaf7f430dc703576
Levi Velazquez <levi@trulii.com>
2015-06-20 23:41:06 -0500

    TRUL-113 #comment Agregado manejo de error en dashboard_instructor. Arreglado error a la hora de guardar la ubicación

--- app/js/utils/services/error.service.js
+++ app/js/utils/services/error.service.js
@@ -43,10 +43,45 @@
             // form.$valid = true;
         }
 
+        function addArrayErrors(form,responseErrors){
+
+
+            // angular.forEach(responseErrors, function (fieldErrors, field) {
+            //     var message = fieldErrors[0];
+
+            //     // Error is unrelated to form fields
+            //     if (field === NON_FIELD_ERRORS) return;
+
+            //     // Process remaining form field errors
+            //     if (field in form){
+            //         form[field].error_message = message;
+            //         form[field].$setValidity(field, false);
+            //     }
+
+            // });
+
+        }
+
         function addErrors(form, responseErrors) {
             
             // form.$valid = false;
             angular.forEach(responseErrors, function (fieldErrors, field) {
+
+                if (_.isArray(fieldErrors)){
+
+
+                    _.each(fieldErrors, function (error_dict) {
+
+                        _.each(error_dict, function (message, field) {
+                            console.log("message,field",message,field);
+                            form[field].error_message = message.pop();
+                            form[field].$setValidity(field, false);
+                        });
+                    });
+                    return
+
+                }
+
                 var message = fieldErrors[0];
 
                 // Error is unrelated to form fields
