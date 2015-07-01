cf6ae1057c3a16ac8f491fe9ec183a9dc918f2db
Fernando De Freitas <fer@trulii.com>
2015-07-01 16:35:25 -0500

    #TRUL-35 #comment Agregado paginador en actividades de perfil organizador

--- app/js/organizers/controllers/profile.controller.js
+++ app/js/organizers/controllers/profile.controller.js
@@ -12,24 +12,28 @@
         var vm = this;
 
         vm.organizer = organizer;
-        vm.activities = activities;
         vm.city = null;
         vm.options = {
             actions: ['view']
         };
+        vm.paginationOpts = {
+            'totalItems': activities.length,
+            'itemsPerPage': 3
+        };
+        console.log('total-items:', vm.paginationOpts.totalItems);
+        console.log('items-per-page:', vm.paginationOpts.itemsPerPage);
 
-        console.log('organizer:', organizer);
+        vm.pageNumber = 1;
+        vm.activities = activities.slice(0, vm.paginationOpts.itemsPerPage);
+        vm.activityPageChange = activityPageChange;
 
         activate();
 
-        function setStrings(){
-            if(!vm.strings){ vm.strings = {}; }
-            angular.extend(vm.strings, {
-                LABEL_PUBLISHED_ACTIVITIES: "Actividades Publicadas",
-                LABEL_BIO: "Biografía",
-                LABEL_CONTACT: "Contactar",
-                COPY_MEMBER_SINCE: "Miembro desde "
-            });
+        function activityPageChange(){
+            var offset = vm.paginationOpts['itemsPerPage'];
+            var start = (vm.pageNumber - 1) * offset;
+            var end = start + offset;
+            vm.activities = activities.slice(start, end);
         }
 
         function getCity(cities, organizer){
@@ -44,6 +48,16 @@
             function isSameCity(city){
                 return city.id === cityId;
             }
+        }
+
+        function setStrings(){
+            if(!vm.strings){ vm.strings = {}; }
+            angular.extend(vm.strings, {
+                LABEL_PUBLISHED_ACTIVITIES: "Actividades Publicadas",
+                LABEL_BIO: "Biografía",
+                LABEL_CONTACT: "Contactar",
+                COPY_MEMBER_SINCE: "Miembro desde "
+            });
         }
 
         function activate(){
