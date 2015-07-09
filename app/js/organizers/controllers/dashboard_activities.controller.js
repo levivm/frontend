2657421f193fdad63d07c780c7083af2fae41f6b
Fernando De Freitas <fer@trulii.com>
2015-07-09 16:17:51 -0500

    #TRUL-81 #in-progress #comment Agregando mensaje de estados vacíos en sección de Actividades de Organizer

--- app/js/organizers/controllers/dashboard_activities.controller.js
+++ app/js/organizers/controllers/dashboard_activities.controller.js
@@ -20,9 +20,9 @@
         var vm = this;
         vm.organizer = organizer;
         vm.isCollapsed = true;
-        vm.published_activities = null;
-        vm.previous_activities = null;
-        vm.draft_activities = null;
+        vm.published_activities = [];
+        vm.previous_activities = [];
+        vm.draft_activities = [];
         vm.currentOptions = {
             actions: ['view', 'edit', 'manage']
         };
@@ -61,6 +61,13 @@
                     + " republicarlas.",
                 COPY_DRAFTS: "Revisa los registros que no has completado o publicado aún en Trulii ¿Qué esperas "
                     + "para publicarlos?",
+                LABEL_EMPTY_PUBLISHED: "No tienes ninguna actividad publicada",
+                COPY_EMPTY_PUBLISHED: "Parece ser el momento perfecto para pulir y publicar una actividad que tengas como borrador, "
+                    + " republicar una actividad pasada o crear una desde cero",
+                LABEL_EMPTY_PREVIOUS: "No tienes actividades pasadas",
+                COPY_EMPTY_PREVIOUS: "Ninguna de tus actividades publicadas se han vencido hasta ahora",
+                LABEL_EMPTY_DRAFTS: "Actualmente no tienes borradores de actividades",
+                COPY_EMPTY_DRAFTS: "Parece ser el momento perfecto para crear y publicar una nueva actividad",
                 SECTION_ACTIVITIES: "Mis Actividades",
                 TAB_PUBLISHED: "Publicadas",
                 TAB_PREVIOUS: "Anteriores",
@@ -72,19 +79,19 @@
             setStrings();
             console.log('activities:', activities);
             activities.map(mapMainPicture);
-            vm.previous_activities = _.filter(activities, function (activity) {
-                var today = new Date();
-                today.setHours(0, 0, 0, 0);
-
-                if (!(activity.last_date)) return false;
-
-                var activity_date = new Date(activity.last_date);
-                activity_date.setHours(0, 0, 0, 0);
-                return !!activity.last_date && activity_date < today && activity.published
-            });
-            vm.draft_activities = _.filter(activities, 'published', false);
-            vm.published_activities = _.difference(_.filter(activities, {'enroll_open': true, 'published': true}),
-                vm.previous_activities);
+            //vm.previous_activities = _.filter(activities, function (activity) {
+            //    var today = new Date();
+            //    today.setHours(0, 0, 0, 0);
+            //
+            //    if (!(activity.last_date)) return false;
+            //
+            //    var activity_date = new Date(activity.last_date);
+            //    activity_date.setHours(0, 0, 0, 0);
+            //    return !!activity.last_date && activity_date < today && activity.published
+            //});
+            //vm.draft_activities = _.filter(activities, 'published', false);
+            //vm.published_activities = _.difference(_.filter(activities, {'enroll_open': true, 'published': true}),
+            //    vm.previous_activities);
         }
 
     }
