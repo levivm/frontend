a1989b44f6fc2546163eb5a563691f6a4082bf0c
Fernando De Freitas <fer@trulii.com>
2015-09-01 19:49:06 -0500

    #TRUL-274 #comment Separados estados de router por modulos

--- app/js/organizers/organizers.module.js
+++ app/js/organizers/organizers.module.js
@@ -1,22 +1,189 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('trulii.organizers', [
-      'trulii.organizers.controllers',
-      'trulii.organizers.services',
-      //'trulii.organizers.directives'
-    ]);
+    angular
+        .module('trulii.organizers', [
+            'trulii.organizers.controllers',
+            'trulii.organizers.services',
+            //'trulii.organizers.directives'
+        ])
+        .config(config);
 
 
+    angular
+        .module('trulii.organizers.controllers', ['angularFileUpload']);
 
-  angular
-    .module('trulii.organizers.controllers',['angularFileUpload']);
+    angular
+        .module('trulii.organizers.services', ['ngCookies']);
 
-  angular
-    .module('trulii.organizers.services', ['ngCookies']);
+    // angular
+    //  .module('trulii.organizer.directives',[]);
 
-  // angular
-  //  .module('trulii.organizer.directives',[]);
+    //noinspection JSValidateJSDoc
+    /**
+     * @ngdoc object
+     * @name trulii.organizers.config
+     * @description Organizers Module Config function
+     * @requires ui.router.state.$stateProvider
+     */
+    config.$inject = ['$stateProvider'];
+    function config($stateProvider) {
+        $stateProvider
+            .state('organizer-landing', {
+                url:'/organizers/landing/',
+                controller: 'OrganizerLandingCtrl',
+                controllerAs: 'vm',
+                templateUrl: 'partials/organizers/landing.html',
+                resolve:{
+                    cities: getAvailableCities
+                }
+            })
+            .state('organizer-profile', {
+                url: '/organizers/{organizer_id:int}/profile',
+                controller: 'OrganizerProfileController as profile',
+                templateUrl: 'partials/organizers/profile.html',
+                resolve: {
+                    organizer: getOrganizer,
+                    activities: getOrganizerActivities
+                }
+            })
+            .state('organizer-dashboard', {
+                abstract:true,
+                url:'/organizer/dashboard/',
+                controller: 'OrganizerDashboardCtrl as dash',
+                templateUrl: 'partials/organizers/dashboard.html',
+                resolve:{
+                    cities:getAvailableCities,
+                    organizer : getCurrentOrganizer
+                },
+                data: {
+                    requiredAuthentication : true
+                }
+            })
+            .state('organizer-dashboard.account', {
+                url:'account/',
+                controller: 'OrganizerAccountCtrl as account',
+                templateUrl: 'partials/organizers/dashboard/account.html'
+            })
+            .state('organizer-dashboard.account.password', {
+                url:'password',
+                templateUrl: 'partials/organizers/dashboard/account_password.html'
+            })
+            .state('organizer-dashboard.account.email', {
+                url:'email',
+                templateUrl: 'partials/organizers/dashboard/account_email.html'
+            })
+            .state('organizer-dashboard.account.banking', {
+                url:'banking',
+                templateUrl: 'partials/organizers/dashboard/account_banking.html'
+            })
+            .state('organizer-dashboard.activities', {
+                url:'activities/',
+                controller: 'OrganizerActivitiesCtrl as activities',
+                templateUrl: 'partials/organizers/dashboard/activities.html',
+                resolve: {
+                    activities: getOrganizerActivities
+                }
+            })
+            .state('organizer-dashboard.activities.open', {
+                url:'open',
+                templateUrl: 'partials/organizers/dashboard/activities_open.html'
+            })
+            .state('organizer-dashboard.activities.closed', {
+                url:'closed',
+                templateUrl: 'partials/organizers/dashboard/activities_closed.html'
+            })
+            .state('organizer-dashboard.activities.inactive', {
+                url:'inactive',
+                templateUrl: 'partials/organizers/dashboard/activities_inactive.html'
+            })
+            .state('organizer-dashboard.history', {
+                url:'history',
+                controller: 'OrganizerHistoryCtrl as history',
+                templateUrl: 'partials/organizers/dashboard_history.html'
+            })
+            .state('organizer-dashboard.profile', {
+                url:'profile',
+                controller: 'OrganizerProfileCtrl as profile',
+                templateUrl: 'partials/organizers/dashboard_profile.html'
+            });
+
+        /**
+         * @ngdoc method
+         * @name .#getCurrentOrganizer
+         * @description Retrieves the current logged Organizer from
+         * {@link trulii.organizers.services.OrganizersManager OrganizersManager} Service otherwise returns ``null``
+         * @requires ng.$timeout
+         * @requires ui.router.state.$state
+         * @requires ng.$q
+         * @requires trulii.organizers.services.OrganizersManager
+         * @methodOf trulii.organizers.config
+         */
+        getCurrentOrganizer.$inject = ['$timeout', '$state', '$q', 'OrganizersManager'];
+        function getCurrentOrganizer($timeout, $state, $q, OrganizersManager){
+
+            return OrganizersManager.getCurrentOrganizer().then(success, error);
+
+            function success(organizer){
+                if(organizer){
+                    return organizer;
+                } else {
+                    //$timeout(function() { $state.go('home'); });
+                    return $q.reject();
+                }
+            }
+
+            function error(response){
+                if(response === null){
+                    console.log("getCurrentOrganizer. There is no Authenticated User");
+                } else {
+                    console.log("getCurrentOrganizer. The Authenticated User is not a Organizer");
+                }
+                return $q.reject();
+            }
+        }
+
+        /**
+         * @ngdoc method
+         * @name .#getPayUData
+         * @description Retrieves an Organizer from
+         * {@link trulii.organizers.services.OrganizersManager OrganizersManager} Service with
+         * the provided ``organizer_id`` from $stateParams
+         * @requires ui.router.state.$stateParams
+         * @requires trulii.organizers.services.OrganizersManager
+         * @methodOf trulii.organizers.config
+         */
+        getOrganizer.$inject = ['$stateParams', 'OrganizersManager'];
+        function getOrganizer($stateParams, OrganizersManager) {
+            return OrganizersManager.getPayUData($stateParams.organizer_id);
+        }
+
+        /**
+         * @ngdoc method
+         * @name .#getOrganizerActivities
+         * @description Retrieves an Organizer's Activities from
+         * {@link trulii.activities.services.ActivitiesManager ActivitiesManager} Service with its ID
+         * @requires trulii.activities.services.ActivitiesManager
+         * @requires organizer
+         * @methodOf trulii.organizers.config
+         */
+        getOrganizerActivities.$inject = ['ActivitiesManager','organizer'];
+        function getOrganizerActivities(ActivitiesManager, organizer){
+            return ActivitiesManager.loadOrganizerActivities(organizer.id);
+        }
+
+        /**
+         * @ngdoc method
+         * @name .#getAvailableCities
+         * @description Retrieves all available cities from
+         * {@link trulii.locations.services.LocationManager LocationManager} Service
+         * @requires trulii.locations.services.LocationManager
+         * @methodOf trulii.organizers.config
+         */
+        getAvailableCities.$inject = ['LocationManager'];
+        function getAvailableCities(LocationManager){
+            return LocationManager.getAvailableCities();
+        }
+    }
 
 })();
--- app/js/students/students.module.js
+++ app/js/students/students.module.js
@@ -11,13 +11,133 @@
         .module('trulii.students', [
             'trulii.students.controllers',
             'trulii.students.services'
-        ]);
-
+        ])
+        .config(config);
 
     angular
         .module('trulii.students.controllers', ['angularFileUpload']);
 
     angular
         .module('trulii.students.services', []);
+
+    //noinspection JSValidateJSDoc
+    /**
+     * @ngdoc object
+     * @name trulii.students.config
+     * @description Students Module Config function
+     * @requires ui.router.state.$stateProvider
+     */
+    config.$inject = ['$stateProvider'];
+    function config($stateProvider) {
+        $stateProvider
+            .state('student-dashboard', {
+                abstract:true,
+                url:'/students/dashboard/',
+                controller: 'StudentDashboardCtrl as dash',
+                templateUrl: 'partials/students/dashboard.html',
+                resolve:{
+                    cities:getAvailableCities,
+                    student: getCurrentStudent
+                },
+                data: {
+                    requiredAuthentication : true
+                }
+            })
+            .state('student-dashboard.account', {
+                url:'account',
+                controller: 'StudentAccountCtrl as account',
+                templateUrl: 'partials/students/dashboard_account.html'
+            })
+            .state('student-dashboard.profile', {
+                url:'profile',
+                controller: 'StudentProfileCtrl as profile',
+                templateUrl: 'partials/students/dashboard_profile.html'
+            })
+            .state('student-dashboard.activities', {
+                url:'activities',
+                controller: 'StudentActivitiesCtrl as activities',
+                templateUrl: 'partials/students/dashboard_activities.html',
+                resolve:{
+                    activities: getStudentActivities
+                }
+            })
+            .state('student-dashboard.history', {
+                url:'history',
+                controller: 'StudentHistoryCtrl as history',
+                templateUrl: 'partials/students/dashboard_history.html'
+            });
+
+        /**
+         * @ngdoc method
+         * @name .#getCurrentStudent
+         * @description Retrieves the current logged Student from
+         * {@link trulii.students.services.StudentsManager StudentsManager} Service otherwise returns ``null``
+         * @requires ng.$timeout
+         * @requires ui.router.state.$state
+         * @requires ng.$q
+         * @requires trulii.students.services.StudentsManager
+         * @methodOf trulii.students.config
+         */
+        getCurrentStudent.$inject = ['$timeout', '$state', '$q', 'StudentsManager'];
+        function getCurrentStudent($timeout, $state, $q, StudentsManager){
+
+            return StudentsManager.getCurrentStudent().then(success, error);
+
+            function success(student){
+                if(student){
+                    return student;
+                } else {
+                    $timeout(function() { $state.go('home'); });
+                    return $q.reject();
+                }
+            }
+
+            function error(response){
+                if(response === null){
+                    console.error("getCurrentStudent. There is no Authenticated User");
+                } else {
+                    console.warn("getCurrentStudent. The Authenticated User is not a Student");
+                }
+                return $q.reject();
+            }
+
+        }
+
+        /**
+         * @ngdoc method
+         * @name .#getStudentActivities
+         * @description Retrieves all Activities from
+         * {@link trulii.students.services.StudentsManager StudentsManager} Service
+         * for the specified Student ID
+         * @requires trulii.activities.services.ActivitiesManager
+         * @requires trulii.students.services.StudentsManager
+         * @methodOf trulii.students.config
+         */
+        getStudentActivities.$inject = ['ActivitiesManager', 'StudentsManager'];
+        function getStudentActivities(ActivitiesManager, StudentsManager){
+            return StudentsManager.getCurrentStudent().then(success, error);
+
+            function success(student){
+                return ActivitiesManager.getStudentActivities(student.id);
+            }
+
+            function error(){
+                $q.reject();
+            }
+        }
+
+        /**
+         * @ngdoc method
+         * @name .#getAvailableCities
+         * @description Retrieves all available cities from
+         * {@link trulii.locations.services.LocationManager LocationManager} Service
+         * @requires trulii.locations.services.LocationManager
+         * @methodOf trulii.students.config
+         */
+        getAvailableCities.$inject = ['LocationManager'];
+        function getAvailableCities(LocationManager){
+            return LocationManager.getAvailableCities();
+        }
+    }
 
 })();
