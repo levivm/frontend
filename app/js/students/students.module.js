4495c1cabe10b67837596903c6da272e3ec3ee79
Fernando De Freitas <fer@trulii.com>
2015-11-12 12:21:28 -0500

    #TRUL-20 #comment Dashboard de Estudiante finalmente con secciones separadas en sub-estados

--- app/js/students/students.module.js
+++ app/js/students/students.module.js
@@ -44,9 +44,17 @@
                 }
             })
             .state('student-dashboard.account', {
-                url:'account',
+                url:'account/',
                 controller: 'StudentAccountCtrl as account',
-                templateUrl: 'partials/students/dashboard_account.html'
+                templateUrl: 'partials/students/dashboard/account.html'
+            })
+            .state('student-dashboard.account.password', {
+                url:'password',
+                templateUrl: 'partials/students/dashboard/account_password.html'
+            })
+            .state('student-dashboard.account.email', {
+                url:'email',
+                templateUrl: 'partials/students/dashboard/account_email.html'
             })
             .state('student-dashboard.profile', {
                 url:'profile',
@@ -82,7 +90,7 @@
             })
             .state('student-dashboard.history.orders', {
                 url:'/orders',
-                templateUrl: 'partials/students/dashboard/history.orders.html',
+                templateUrl: 'partials/students/dashboard/history.orders.html'
             })
             .state('student-dashboard.history.orders.order', {
                 url:'/:orderId',
