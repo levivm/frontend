940b0f70610994f2a6bb9f4a13a46b87976b8f72
Fernando De Freitas <fer@trulii.com>
2015-12-02 18:57:19 -0500

    #TRUL-231 #comment Arreglados estilos de 404 con diseño de invision

--- app/js/ui-components/directives/navbar.directive.js
+++ app/js/ui-components/directives/navbar.directive.js
@@ -138,6 +138,7 @@
 
                     unsubscribeStateChange = $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
                         scope.state = toState.name;
+                        scope.isSearchVisible = !(toState.name == 'home' || toState.name == 'not-found');
                     });
 
                     unsubscribeUserChanged = $rootScope.$on(Authentication.USER_CHANGED_EVENT, function (event) {
