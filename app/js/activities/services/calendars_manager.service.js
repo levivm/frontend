d752b4b806273fe9c4bb19c0960fcec8e9be6df0
Fernando De Freitas <fer@trulii.com>
2016-01-02 12:35:27 -0500

    #TRUL-77 #comment Cambios en servicio de CalendarsManager y Activity

--- app/js/activities/services/calendars_manager.service.js
+++ app/js/activities/services/calendars_manager.service.js
@@ -15,150 +15,193 @@
         .module('trulii.activities.services')
         .factory('CalendarsManager', CalendarsManager);
 
-    CalendarsManager.$inject = ['$http', '$q', '$filter', 'ActivityServerApi', 'Calendar'];
+    CalendarsManager.$inject = ['$http', '$q', 'ActivityServerApi', 'Calendar'];
 
-    function CalendarsManager($http, $q, $filter, ActivityServerApi, Calendar) {
+    function CalendarsManager($http, $q, ActivityServerApi, Calendar) {
 
         var api = ActivityServerApi;
+        var _pool = {};
+        var calendars = [];
+        var activityId = null;
+
         //noinspection UnnecessaryLocalVariableJS
         var CalendarsManager = {
-            _pool : {},
-            calendars : [],
-            _retrieveInstance : function (calendarId, calendarData) {
-                //console.log('pool', this._pool);
-                var instance = this._search(calendarId);
-                // if calendar does not exist, create new one using calendarData
-                if (!(instance)) {
-                    instance = new Calendar(calendarData);
-                    instance.activity = this.activity_id;
-                }
-                else{
-                    // if calendar exists and calendarData is not NULL, update the calendar
-                    if(calendarData)
-                        instance.setData(calendarData);
-                }
 
-                return instance;
-            },
-            _search : function (calendarId) {
-                return this._pool[calendarId];
-            },
-            _deleteInsntance : function (calendarId) {
+            calendars: calendars,
 
-                var scope = this;
-                var instance = scope._search(calendarId);
-                var calendars = angular.copy(scope.calendars);
-                if (instance) {
-                    angular.forEach(calendars, function (calendar, index) {
+            /**
+             * @ngdoc function
+             * @name .#deleteCalendar
+             * @description Deletes a Calendar from an Activity
+             * @param {number} calendarId Id of the Calendar to delete
+             * @methodOf trulii.activities.services.CalendarsManager
+             */
+            deleteCalendar: deleteCalendar,
 
-                        if (calendar.id == instance.id) {
-                            scope.calendars.splice(index, 1);
-                        }
+            /**
+             * @ngdoc function
+             * @name .#deleteCalendar
+             * @description Retrieves a Calendar instance from an Activity
+             * @param {number} calendarId Id of the Calendar to retrieve
+             * @methodOf trulii.activities.services.CalendarsManager
+             */
+            getCalendar: getCalendar,
 
-                    });
-                    delete scope._pool[calendarId];
+            /**
+             * @ngdoc function
+             * @name .#fetchCalendar
+             * @description Retrieves a Calendar from an Activity directly from server
+             * @param {number} calendarId Id of the Calendar to retrieve
+             * @methodOf trulii.activities.services.CalendarsManager
+             */
+            fetchCalendar: fetchCalendar,
 
-                }
+            /**
+             * @ngdoc function
+             * @name .#fetchCalendar
+             * @description Retrieves a Calendar from an Activity directly from server
+             * @param {number} activityId Id of the Activity to get calendars from
+             * @param {boolean} active flag
+             * @methodOf trulii.activities.services.CalendarsManager
+             */
+            loadCalendars: loadCalendars,
 
-            },
-            deleteCalendar : function (calendarId) {
-
-                var deferred = $q.defer();
-                var calendar = this._search(calendarId);
-
-                var scope = this;
-
-                return $http.delete(api.calendar(this.activity_id, calendarId))
-                    .then(function (response) {
-                        scope._deleteInsntance(calendarId);
-                        deferred.resolve(scope.calendars);
-                        return deferred.promise;
-                    },
-                    function (response) {
-                        return $q.reject(response.data);
-                    });
-
-            },
-            getCalendar : function (calendarId) {
-                var deferred = $q.defer();
-                var calendar = new Calendar();
-                angular.extend(calendar, this._retrieveInstance(calendarId));
-                deferred.resolve(calendar);
-                return deferred.promise;
-            },
-            fetchCalendar : function (activityId, calendarId) {
-                var scope = this;
-                this.activity_id = activityId;
-                var deferred = $q.defer();
-                $http.get(api.calendar(activityId, calendarId))
-                    .success(function (result) {
-                        var calendar = scope._retrieveInstance(result.id, result);
-                        deferred.resolve(calendar);
-                    })
-                    .error(function (result) {
-                        deferred.reject(result);
-                    });
-
-                return deferred.promise;
-            },
-            loadCalendars : function (activity_id, active) {
-                var actives = active || false;
-                this.activity_id = activity_id;
-                var scope = this;
-
-                if (scope.calendars.length > 0) {
-                    var activity_calendar_id = scope.calendars[0].activity;
-                    if (activity_calendar_id == this.activity_id){ return scope.calendars; }
-                }
-
-                if (actives) { config.actives = true; }
-
-                return $http.get(api.calendars(this.activity_id))
-                    .then(function (response) {
-                        console.log('CalendarsManagercalendars response:', response);
-                        scope._setCalendars(response.data);
-                        return scope.calendars;
-                    },
-                    function (response) {
-                        return response.data;
-                    });
-            },
-
-            _setCalendars : function (calendarsData) {
-
-                var scope = this;
-                    scope.calendars  = [];
-                angular.forEach(calendarsData, function (calendarData) {
-                    var calendar = new Calendar(calendarData);
-                    scope._addCalendar(calendar);
-                    // scope.calendars.push(calendar);
-                    // scope._pool[calendar.id] = calendar;
-
-                });
-
-            },
-            _addCalendar : function (calendar) {
-                this._pool[calendar.id] = calendar;
-                this.calendars.push(calendar);
-
-            },
-            setCalendar : function (calendarData) {
-                var scope = this;
-                var calendar = this._search(calendarData.id);
-
-                if (calendar) {
-                    calendar.setData(calendarData);
-                } else {
-                    calendar = scope._retrieveInstance(calendarData.id, calendarData);
-                    this._addCalendar(calendar);
-
-                }
-                return calendar;
-            }
-
+            /**
+             * @ngdoc function
+             * @name .#setCalendar
+             * @description Sets a Calendar
+             * @param {object} calendarData calendar data to extend to calendar instance
+             * @methodOf trulii.activities.services.CalendarsManager
+             */
+            setCalendar: setCalendar
         };
 
         return CalendarsManager;
+
+        //--------- Exposed Functions ---------//
+
+        function deleteCalendar(calendarId) {
+            var deferred = $q.defer();
+
+            $http.delete(api.calendar(activityId, calendarId)).then(success, error);
+
+            return deferred.promise;
+
+            function success() {
+                _deleteInstance(calendarId);
+                deferred.resolve(calendars);
+            }
+            function error(response) {
+                deferred.reject(response.data);
+            }
+        }
+
+        function getCalendar(calendarId) {
+            var deferred = $q.defer();
+            var calendar = new Calendar();
+
+            angular.extend(calendar, _retrieveInstance(calendarId));
+            deferred.resolve(calendar);
+
+            return deferred.promise;
+        }
+
+        function fetchCalendar(activity_id, calendarId) {
+            var deferred = $q.defer();
+            activityId = activity_id;
+
+            $http.get(api.calendar(activityId, calendarId)).then(success, error);
+            return deferred.promise;
+
+            function success(response) {
+                var result = response.data;
+                var calendar = _retrieveInstance(result.id, result);
+                deferred.resolve(calendar);
+            }
+
+            function error(response) {
+                var result = response.data;
+                deferred.reject(result);
+            }
+        }
+
+        function loadCalendars(activity_id, active) {
+            var actives = active || false;
+            activityId = activity_id;
+
+            if (calendars.length > 0) {
+                var activity_calendar_id = calendars[0].activity;
+                if (activity_calendar_id == activityId){ return calendars; }
+            }
+
+            if (actives) { config.actives = true; }
+
+            return $http.get(api.calendars(activityId)).then(success, error);
+
+            function success(response) {
+                console.log('CalendarsManager. Calendars response:', response);
+                _setCalendars(response.data);
+                return calendars;
+            }
+
+            function error(response) {
+                return response.data;
+            }
+        }
+
+        function setCalendar(calendarData) {
+            var calendar = _search(calendarData.id);
+
+            if (calendar){
+                calendar.setData(calendarData);
+            } else {
+                calendar = _retrieveInstance(calendarData.id, calendarData);
+                _addCalendar(calendar);
+            }
+
+            return calendar;
+        }
+
+        //--------- Internal Functions ---------//
+
+        function _retrieveInstance(calendarId, calendarData) {
+            var instance = _search(calendarId);
+            // if calendar does not exist, create new one using calendarData
+            if (!instance){
+                instance = new Calendar(calendarData);
+                instance.activity = activityId;
+            } else {
+                // if calendar exists and calendarData is not NULL, update the calendar
+                if(calendarData){ instance.setData(calendarData); }
+            }
+
+            return instance;
+        }
+
+        function _search(calendarId) {
+            return _pool[calendarId];
+        }
+
+        function _deleteInstance(calendarId) {
+            var instance = _search(calendarId);
+            if (instance){
+                angular.forEach(calendars, function (calendar, index) {
+                    if (calendar.id == instance.id){ calendars.splice(index, 1); }
+                });
+                delete _pool[calendarId];
+            }
+        }
+        function _setCalendars(calendarsData) {
+            angular.forEach(calendarsData, function (calendarData) {
+                var calendar = new Calendar(calendarData);
+                _addCalendar(calendar);
+            });
+        }
+
+        function _addCalendar(calendar) {
+            _pool[calendar.id] = calendar;
+            calendars.push(calendar);
+        }
     }
 
 })();
