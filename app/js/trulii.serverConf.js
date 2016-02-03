ccd2b59ef22302a843c60d02cb73863724d2dbaa
Andres De Freitas <adefreitas12@gmail.com>
2016-02-03 17:09:51 -0430

    TRUL-341 #comment cambios de activity review para mobile

--- app/js/trulii.serverConf.js
+++ app/js/trulii.serverConf.js
@@ -1,5 +1,5 @@
 (function () { 
  return angular.module('trulii.routes.config', [])
-.constant('serverConf', {"url":"http://192.168.1.107:8000","FACEBOOK_APP_KEY":"1701354690078591"});
+.constant('serverConf', {"url":"http://192.168.1.104:8000","FACEBOOK_APP_KEY":"1701354690078591"});
 
 })();
