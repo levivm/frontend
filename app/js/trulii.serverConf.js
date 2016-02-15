53f40da90497c6e0b2667501099265b4db2bed69
Andres De Freitas <adefreitas12@gmail.com>
2016-02-15 18:12:33 -0430

    merge

--- app/js/trulii.serverConf.js
+++ app/js/trulii.serverConf.js
@@ -1,5 +1,5 @@
 (function () { 
  return angular.module('trulii.routes.config', [])
-.constant('serverConf', {"url":"http://192.168.1.115:8000","FACEBOOK_APP_KEY":"1701354690078591","s3URL":"https://s3-us-west-2.amazonaws.com/trulii-dev"});
+.constant('serverConf', {"url":"http://localhost:8000","FACEBOOK_APP_KEY":"1701354690078591","s3URL":"https://s3-us-west-2.amazonaws.com/trulii-dev"});
 
 })();
