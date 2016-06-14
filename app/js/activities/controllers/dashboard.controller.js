5a7158041c0c6f7f89671b8323e2c259b6915f21
HenryGBC <henry.bravo89@gmail.com>
2016-06-14 11:18:02 -0430

    TRUL-84 #comment Sidebar position dinamyc

--- app/js/activities/controllers/dashboard.controller.js
+++ app/js/activities/controllers/dashboard.controller.js
@@ -116,7 +116,49 @@
             });
         }
 
-
+        function _initWidget(){
+            var ctrlFooter = false;
+            var positionStyle = 'absolute';
+            var valuePosition = '210px';
+            angular.element(document).ready(function () {
+                $scope.$on('scrolled',
+                  function(scrolled, scroll){
+                    var sideBarPosition = (document.getElementsByClassName('sidebar-edit-activity')[0].getBoundingClientRect().top + window.scrollY) ;
+                    var footerPosition = document.getElementsByClassName('container-fluid')[0].offsetHeight - 80 ;
+                    var coverPosition = (document.getElementsByClassName('cover-edit-activity')[0].getBoundingClientRect().top + window.scrollY) + document.getElementsByClassName('cover-edit-activity')[0].offsetHeight;
+                    var navBarHeight = document.getElementsByClassName('navbar')[0].offsetHeight;
+                    var sidebarTop =  document.getElementsByClassName('sidebar-edit-activity')[0].getBoundingClientRect().top - navBarHeight;
+                    var positionToFixed = window.scrollY +  document.getElementsByClassName('sidebar-edit-activity')[0].offsetHeight;
+               
+                    if(sidebarTop <= 20){
+                       if(sideBarPosition <= coverPosition){
+                            positionStyle = 'absolute';
+                            valuePosition = '210px';
+                            ctrlFooter = false;
+                        }else{
+                            if( positionToFixed >= footerPosition ){
+                                positionStyle = 'absolute';
+                                valuePosition = footerPosition-document.getElementsByClassName('sidebar-edit-activity')[0].offsetHeight+'px';
+                                ctrlFooter = true;
+                            }else{
+                                positionStyle = 'fixed';
+                                valuePosition = '90px';
+                            }
+                        }  
+                    }else{
+                       if( positionToFixed <= footerPosition && ctrlFooter){
+                            positionStyle = 'fixed';
+                            valuePosition = '90px';
+                        }
+                       
+                    }
+                    document.getElementsByClassName('sidebar-edit-activity')[0].style.position = positionStyle;
+                    document.getElementsByClassName('sidebar-edit-activity')[0].style.top = valuePosition;
+                    $scope.$apply();
+                  }
+                );
+            });
+        }
 
         function _setStrings(){
 
@@ -146,7 +188,8 @@
 
         function activate() {
             _setStrings();
-            _initScroll();
+            //_initScroll();
+            _initWidget();
             // pc.sidebar = true;
             activity.updateAllSections();
 
