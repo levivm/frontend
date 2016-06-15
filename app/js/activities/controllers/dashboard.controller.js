a50298ebf5e72a62d457498ebaab0bed10ff30be
HenryGBC <henry.bravo89@gmail.com>
2016-06-15 18:20:42 -0430

    TRUL-84 #comment preview button y sidebar

--- app/js/activities/controllers/dashboard.controller.js
+++ app/js/activities/controllers/dashboard.controller.js
@@ -124,21 +124,22 @@
                 $scope.$on('scrolled',
                   function(scrolled, scroll){
                     var sideBarPosition = (document.getElementsByClassName('sidebar-edit-activity')[0].getBoundingClientRect().top + window.scrollY) ;
-                    var footerPosition = document.getElementsByClassName('container-fluid')[0].offsetHeight - 80 ;
+                    var footerPosition = document.getElementsByClassName('container-fluid')[0].offsetHeight +80 ;
                     var coverPosition = (document.getElementsByClassName('cover-edit-activity')[0].getBoundingClientRect().top + window.scrollY) + document.getElementsByClassName('cover-edit-activity')[0].offsetHeight;
                     var navBarHeight = document.getElementsByClassName('navbar')[0].offsetHeight;
                     var sidebarTop =  document.getElementsByClassName('sidebar-edit-activity')[0].getBoundingClientRect().top - navBarHeight;
                     var positionToFixed = window.scrollY +  document.getElementsByClassName('sidebar-edit-activity')[0].offsetHeight;
-               
+                    console.log('px', positionToFixed);
+                    console.log('fp', footerPosition);
                     if(sidebarTop <= 20){
                        if(sideBarPosition <= coverPosition){
                             positionStyle = 'absolute';
-                            valuePosition = '210px';
+                            valuePosition = '0px';
                             ctrlFooter = false;
                         }else{
                             if( positionToFixed >= footerPosition ){
                                 positionStyle = 'absolute';
-                                valuePosition = footerPosition-document.getElementsByClassName('sidebar-edit-activity')[0].offsetHeight+'px';
+                                valuePosition = footerPosition-document.getElementsByClassName('sidebar-edit-activity')[0].offsetHeight-180+'px';
                                 ctrlFooter = true;
                             }else{
                                 positionStyle = 'fixed';
