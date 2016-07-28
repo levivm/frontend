ffaed797d4b52eb3d7a4211ffbfb4b110410c648
HenryGBC <henry.bravo89@gmail.com>
2016-07-28 18:40:52 -0430

    TRUL-348 #comment CalendarTests

--- tests/activities/controllers/dashboard_calendars.controllers.spec.js
+++ tests/activities/controllers/dashboard_calendars.controllers.spec.js
@@ -9,6 +9,8 @@ xdescribe('Controller: ActivityCalendarsController', function(){
         $httpBackend,
         currentUser,
         presaveInfo,
+        calendarData,
+        $modal
         scope = {};
 
     beforeEach(function(){
@@ -73,14 +75,20 @@ xdescribe('Controller: ActivityCalendarsController', function(){
             }, function(response){
                 console.log(response);
         });
+       
+        CalendarsManager.getCalendar(null, 4)
+            .then(function(data){
+                calendarData = data;
 
+            }, function(response){
+                console.log(response);
+            });        
 
 
        
         currentUser = readJSON('tests/mock/currentUser.json');
         //End calls
         $httpBackend.flush();
-
         ActivityCalendarsController =  $controller('ActivityCalendarsController', {
             'activity': activity,
             'organizer': organizer,
@@ -94,6 +102,7 @@ xdescribe('Controller: ActivityCalendarsController', function(){
 
     describe("Initializacion", function(){
         it('should have controller defined and strings is object defined', function() {
+            console.log(ActivityCalendarsController);
             expect(ActivityCalendarsController).toBeDefined();
             expect(ActivityCalendarsController.strings).toBeDefined();
          });
@@ -102,9 +111,35 @@ xdescribe('Controller: ActivityCalendarsController', function(){
              expect(ActivityCalendarsController.calendars.length).toBe(4);
          });
     });
-
     
+    describe("Set ", function(){
+         it('createCalendar() function successfully', inject(function($state){
+        // Call something that eventually hits $state.go
+            spyOn($state, 'go');
+            $state.go('.detail', {'calendar_id' : null});
+            ActivityCalendarsController.createCalendar();
+            expect($state.go).toHaveBeenCalled();
+        }));
+        
+        it('setCalendar() function successfully', inject(function($state){
+        // Call something that eventually hits $state.go
+        
+            var calendar = readJSON('tests/mock/calendar_sessions.json');
+            spyOn($state, 'go');
+            $state.go("^");
+            ActivityCalendarsController.setCalendar(calendar);
+            expect($state.go).toHaveBeenCalled();
+        }));
+        
+         it('deleteCalendar() function successfully', function(){
+        // Call something that eventually hits $state.go
+            console.log(ActivityCalendarsController);
+            calendarData.assistants =[];
+            ActivityCalendarsController.deleteCalendar(calendarData);
+        });
 
+         
+    });
 
 
 
