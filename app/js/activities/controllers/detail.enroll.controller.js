7062e2fc2298cdd4e34e6bd5168fbbf417ce4c74
Levi Velazquez <levi@trulii.com>
2016-09-23 17:23:02 -0500

    #hotfix agregado package id en pago con PSE

--- app/js/activities/controllers/detail.enroll.controller.js
+++ app/js/activities/controllers/detail.enroll.controller.js
@@ -368,7 +368,7 @@
             };
 
             if(activity.is_open){
-                data.package_id = vm.package;
+                data.package_id = vm.package.id;
             }
             ActivitiesManager.enroll(activity.id, data).then(_enrollSuccess, _enrollError)
                               .finally(_finishProccesingPayment);
