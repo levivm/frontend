eefddb6d2a60cd8e444872b9950d0d43f0c77873
HenryGBC <henry.bravo89@gmail.com>
2016-09-24 21:50:41 -0430

    Hotfix font-size

--- app/js/locations/services/locations_manager.service.js
+++ app/js/locations/services/locations_manager.service.js
@@ -136,10 +136,8 @@
         }
 
         function getCityById(city_id) {
-            getAvailableCities().then(function(cities){
-               availableCities = cities;
-               return availableCities.filter(byId)[0];
-            });
+            return availableCities.filter(byId)[0];
+            
             function byId(city) {
                 return city.id === city_id;
             }
